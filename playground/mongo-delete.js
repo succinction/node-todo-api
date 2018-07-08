0ac79382bf28bf0f31c929958818a78ac15a095d
Joe Howard <succinction@gmail.com>
2018-07-07 17:40:06 -0700

    mongo delete

---
+++ playground/mongo-delete.js
@@ -0,0 +1,34 @@
+const { MongoClient, Object } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) return console.log("ERROR: could not connect to the database.<<<:", err)
+    console.log('Connected to the database.')
+
+    const db = client.db('TodoApp')
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({ game: 234 }).then((result) => {
+        // console.log(result)
+    // }, (err) => {
+    //     console.log("ERROR:", err)
+    // })
+
+    // // deleteOne
+    // db.collection('Todos').deleteOne({ user: 'Joe' }).then((result) => {
+    //     console.log(result)
+    // }, (err) => {
+    //     console.log("ERROR:", err)
+    // })
+
+    // // findOneAndDelete
+    db.collection('Users').findOneAndDelete({ name: 'Jill' })
+    .then((result) => {
+        console.log(result)
+    }, (err) => {
+        console.log("ERROR:", err)
+    })
+
+    client.close()
+})
+
+
