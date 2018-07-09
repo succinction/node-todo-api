18c3aa666dfb68dbd5463ee27ed069a3144e62ca
Joe Howard <succinction@gmail.com>
2018-07-08 23:34:56 -0700

    better config

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,23 @@
 var env = process.env.NODE_ENV || 'development'
 
-if (env === 'development') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-}else if (env === 'test') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json')
+    var envConfig = config[env]
+    // console.log(Object.keys(envConfig))
+    for (let key of Object.keys(envConfig)) {
+        process.env[key] = envConfig[key]
+    }
+
 }
+
+
+
+// if (env === 'development') {
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+// }else if (env === 'test') {
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+// }
 
--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this
     var access = 'auth'
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'secret_salt')
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat([{ access, token }])
 
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {
     var User = this
     var decoded
     try {
-        decoded = jwt.verify(token, 'secret_salt')
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
     } catch (e) {
         console.log("ERROR:", e)
         return Promise.reject()
